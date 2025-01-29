4a7cd2b2e71508424f974a1996a107c0a4467b1e
RomoTOYS <antonioromo032@gmail.com>
2025-01-29 13:39:37 -0600

    Delete scrip.js

--- scrip.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hearts = document.querySelector(".hearts");
-    let scale = 1;
-
-    setInterval(() => {
-        scale = scale === 1 ? 1.2 : 1;
-        hearts.style.transform = `scale(${scale})`;
-    }, 500);
-});
